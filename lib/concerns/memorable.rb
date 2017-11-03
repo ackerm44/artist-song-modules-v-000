77414fd2b3c3ee866ea2195aef0aaa3235083358
Amanda Ackerman <ackerm44@gmail.com>
2017-11-03 20:06:15 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,5 @@
 module Memorable
-  #module ClassMethods
+  module ClassMethods
 
     def reset_all
       self.all.clear
@@ -9,5 +9,11 @@ module Memorable
       self.all.count
     end
 
-  #end
+  end
+
+  module InstanceMethods
+    def initialize
+    end
+  end
+  
 end
