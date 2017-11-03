630dab69da3c0656bedb96b973fb05318c8d6917
Amanda Ackerman <ackerm44@gmail.com>
2017-11-03 18:36:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,10 +8,10 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@all = []
+  @@songs = []
 
   def initialize
-    @@all << self
+    @@songs << self
   end
 
   # def self.find_by_name(name)
@@ -27,6 +27,6 @@ class Song
   # end
 
   def self.all
-    @@all
+    @@songs
   end
 end
