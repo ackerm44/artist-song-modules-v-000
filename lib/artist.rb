271c3bfcdb3a0b7c8099e1bd1c171b19ad6cbe0e
Amanda Ackerman <ackerm44@gmail.com>
2017-11-03 18:34:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,19 +8,19 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@artists = []
+  @@all = []
 
   # def self.find_by_name(name)
   #   @@artists.detect{|a| a.name == name}
   # end
 
   def initialize
-    @@artists << self
+    @@all << self
     @songs = []
   end
 
   def self.all
-    @@artists
+    @@all
   end
 
   def add_song(song)
